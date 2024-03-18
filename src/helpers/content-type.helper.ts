cf28241f8bdc6bc508e095f8e0e4286cb210f772
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-03-18 17:07:17 -0300

    Create helper to identity the content-type

---
+++ src/helpers/content-type.helper.ts
@@ -0,0 +1,28 @@
+export function getContentType(buffer: Buffer): string | null {
+  if (buffer.length < 2) {
+    return null;
+  }
+
+  if (buffer[0] === 0xFF && buffer[1] === 0xD8) {
+    return 'image/jpeg';
+  }
+
+  if (
+    buffer[0] === 0x89 &&
+    buffer[1] === 0x50 &&
+    buffer[2] === 0x4E &&
+    buffer[3] === 0x47 &&
+    buffer[4] === 0x0D &&
+    buffer[5] === 0x0A &&
+    buffer[6] === 0x1A &&
+    buffer[7] === 0x0A
+  ) {
+    return 'image/png';
+  }
+
+  if (buffer[0] === 0x47 && buffer[1] === 0x49 && buffer[2] === 0x46) {
+    return 'image/gif';
+  }
+
+  return null;
+}
