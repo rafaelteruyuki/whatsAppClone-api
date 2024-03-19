daef0523bb5f5e9b7dc422dd719b3cd69e856306
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-03-19 16:50:09 -0300

    Add support for initials

--- src/user/User.ts
+++ src/user/User.ts
@@ -3,9 +3,14 @@ import { v4 as uuid } from 'uuid';
 export class User {
   private id: string;
   private name: string;
+  private initials: string;
 
   constructor(name: string) {
     this.id = uuid();
     this.name = name;
+    this.initials = name.split(' ').reduce((prev, curr) => {
+      const initial = curr.split('')[0].toUpperCase();
+      return `${prev}${initial}`
+    }, '');
   }
 }
